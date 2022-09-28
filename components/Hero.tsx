9865484429e7204de2b289d86821d80a0700e665
Hreiðar Ingi <hreidar220@gmail.com>
2022-09-28 20:43:19 +0000

    minor fix

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,18 +3,17 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import { urlFor } from "../sanity";
 import { PageInfo } from "../typings";
-// import { urlFor } from "../sanity";
-import BackgroundCirles from "./BackgroundCirles";
+import BackgroundCircles from "./BackgroundCircles";
 
 type Props = {
   pageInfo: PageInfo;
 };
 
-export default function Hero({ pageInfo }: Props) {
+function Hero({ pageInfo }: Props) {
   const [text, count] = useTypewriter({
     words: [
-      `Hi, my name is ${pageInfo?.name}`,
-      "Guy-who-loves-coffee.tsx",
+      `Hi, The name's ${pageInfo?.name}`,
+      "Guy-who-loves-Coffee.tsx",
       "<ButLovesToCodeMore />",
     ],
     loop: true,
@@ -22,14 +21,14 @@ export default function Hero({ pageInfo }: Props) {
   });
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-      <BackgroundCirles />
+      <BackgroundCircles />
       <img
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
         src={urlFor(pageInfo?.heroImage).url()}
         alt=""
       />
-      <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+      <div className="z-20 relative">
+        <h2 className="tracking-[15px] text-sm uppercase text-gray-500 pb-2">
           {pageInfo?.role}
         </h2>
         <h1 className="text-5xl lg:text-6xl font-semibold px-10">
@@ -41,12 +40,15 @@ export default function Hero({ pageInfo }: Props) {
           <Link href="#about">
             <button className="heroButton">About</button>
           </Link>
+
           <Link href="#experience">
             <button className="heroButton">Experience</button>
           </Link>
+
           <Link href="#skills">
             <button className="heroButton">Skills</button>
           </Link>
+
           <Link href="#projects">
             <button className="heroButton">Projects</button>
           </Link>
@@ -55,3 +57,5 @@ export default function Hero({ pageInfo }: Props) {
     </div>
   );
 }
+
+export default Hero;
